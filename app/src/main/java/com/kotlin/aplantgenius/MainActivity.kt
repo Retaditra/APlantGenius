0931907c07f3519b5343aada42c7fe371f124045
Adi <retnoadisaputra57@gmail.com>
2023-05-22 08:19:07 +0700

    camera & diseases

--- app/src/main/java/com/kotlin/aplantgenius/MainActivity.kt
+++ app/src/main/java/com/kotlin/aplantgenius/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.kotlin.aplantgenius
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kotlin.aplantgenius.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.started.setOnClickListener {
+            val intent = Intent(this, DiseasesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
