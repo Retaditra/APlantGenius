881041b0d22b693e40986786cbbacf27e02a5ee2
Adi <retnoadisaputra57@gmail.com>
2023-05-22 09:56:15 +0700

    camera & diseases

--- app/src/main/java/com/kotlin/aplantgenius/MainActivity.kt
+++ app/src/main/java/com/kotlin/aplantgenius/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.kotlin.aplantgenius
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.kotlin.aplantgenius.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.started.setOnClickListener {
-            val intent = Intent(this, DiseasesActivity::class.java)
-            startActivity(intent)
-        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomBar.setupWithNavController(navController)
     }
 }
